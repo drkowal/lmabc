c161231c7535cefadf2a26f674dde4029ca3889c
Prayag Gordy <prayaggordy@gmail.com>
2022-06-21 00:54:47 -0400

    comment out BIC for now... giving build issues

--- R/model_selection.R
+++ R/model_selection.R
@@ -3,7 +3,7 @@ AIC.lm.abc = function(fit,...){
 	stats::AIC(fit$lm,...)
 }
 
-#' @export
-BIC.lm.abc = function(fit,...){
-	stats::BIC(fit$lm,...)
-}
+#' #' @export
+#' BIC.lm.abc = function(fit,...){
+#' 	stats::BIC(fit$lm,...)
+#' }
