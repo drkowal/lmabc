7d417c4fb093eab6044e0f650c4599b9f44d5e24
Prayag Gordy <prayaggordy@gmail.com>
2022-06-21 01:02:23 -0400

    something is wrong with BIC

--- R/model_selection.R
+++ R/model_selection.R
@@ -3,7 +3,8 @@ AIC.lm.abc = function(object,...,k=2){
 	stats::AIC(object$lm,...,k=k)
 }
 
-#' #' @export
-#' BIC.lm.abc = function(object,...){
-#' 	stats::BIC(object$lm,...)
-#' }
+#' @method BIC lm.abc
+#' @export
+BIC.lm.abc = function(object,...){
+	stats::BIC(object$lm,...)
+}
