2a6c1da69c2eeff5c9ed4da298ac4349258d78f6
Jai Uparkar <91229562+jai-uparkar@users.noreply.github.com>
2022-06-28 15:27:38 -0500

    summary(fit_abc) and summary(fit_base) have same output but summary.lm(fit_abc) has the correct output. Function is not working but this is the only issue. Also wrote the description for the function.

--- R/summary.R
+++ R/summary.R
@@ -1,9 +1,22 @@
+#' Summarizing Linear ABC Model Fits
+#'
+#' \code{summary.lmbc} summary method for class "lmabc".
+#'
+#' This function uses the summary.lm functionality. To learn more about the original
+#' functionality go to \code{\link[stats]{summary.lm}}.
+
+#' @param object An object of class "lmabc", usually, a direct result of a call to lmabc
+#' @inheritParams stats::summary.lm
+#'
+#' @return A list of summary statistics of the fitted linear ABC
+#' model given in object.
+
 #' @export
+
 summary.lmabc = function(object, ...){
 	# fixme!
-	print("heloo")
 	ses = sqrt(diag(vcov(object)))
-	summary(object$lm) # new line here
+	summary.lm(object$lm, ...) # new line here
 }
 
 #' @export
