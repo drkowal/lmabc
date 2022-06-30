5124d7c7b0569c1c0377fc74068fa06f2b32b91f
Prayag Gordy <prayaggordy@gmail.com>
2022-06-30 19:11:09 -0400

    some cleanup

--- R/summary.R
+++ R/summary.R
@@ -8,20 +8,17 @@
 #' @returns A list of summary statistics of the fitted linear ABC model given in object.
 #' @seealso [stats::summary.lm]
 #' @export
-summary.lmabc = function(object, correlation = FALSE, symbolic.cor = FALSE, ...){
-	# New coefficient matrix
+summary.lmabc <- function(object, correlation = FALSE, symbolic.cor = FALSE, ...) {
 	summary_base <- summary(object$lm) # a lot of the information is the same between the base summary and the abc summary
 
-	ses = sqrt(diag(vcov(object))) # Calculating the new standard error
-	term_coeff = object$coefficients # extracting all the coefficents from the lmabc model
-	t_val = term_coeff/ses # New t-values
-	p_val = 2*pt(abs(t_val), object$df.residual, lower.tail = FALSE) # new probabilities calculated here
+	ses <- sqrt(diag(vcov(object))) # Calculating the new standard error
+	term_coeff <- object$coefficients # extracting all the coefficents from the lmabc model
+	t_val <- term_coeff/ses # New t-values
+	p_val <- 2*pt(abs(t_val), object$df.residual, lower.tail = FALSE) # new probabilities calculated here
 
 	coefficients_abc <- cbind(term_coeff, ses, t_val, p_val) # entering the new values of the coeff matrix
 	# renaming the dimensions of the new coeff matrix
-	dimnames(coefficients_abc)<-
-		list(names(term_coeff),
-				 c("Estimate", "Std. Error", "t value", "Pr(>|t|)"))
+	dimnames(coefficients_abc) <- list(names(term_coeff), c("Estimate", "Std. Error", "t value", "Pr(>|t|)"))
 
 	summary_abc <- summary_base; attr(summary_abc, 'class') <- "summary.lmabc"
 	summary_abc$summary.lm <- summary_base
