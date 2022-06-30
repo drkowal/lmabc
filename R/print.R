12f5b3fed55229254242456ab4a216dbfe5ca6e4
Prayag Gordy <prayaggordy@gmail.com>
2022-06-30 12:48:50 -0400

    simple call to print.lm does the trick

--- R/print.R
+++ R/print.R
@@ -1,11 +1,4 @@
+#' @export
 print.lmabc <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
-	cat("\nCall:\n",
-			paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
-	if(length(coef(x))) {
-		cat("Coefficients:\n")
-		print.default(format(coef(x), digits = digits),
-									print.gap = 2L, quote = FALSE)
-	} else cat("No coefficients\n")
-	cat("\n")
-	invisible(x)
+	stats:::print.lm(x = x, digits = digits, ...)
 }
