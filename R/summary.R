aecbca643a71b0107268a6efacbad03e6a04f870
Jai Uparkar <91229562+jai-uparkar@users.noreply.github.com>
2022-06-30 15:59:53 -0500

    Fixed the coeff matrix for lmabc summary and now working on the correlation matrix. The logic follows that of the original summary.lm but I'm getting an dimensional issue when running line 35. Not sure if I'm overlooking a math issue or if the problem lays in my code. Refer to lin 342-346 for the implemention of correlation = TRUE on https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/lm.R#L261

--- R/summary.R
+++ R/summary.R
@@ -8,17 +8,38 @@
 #' @returns A list of summary statistics of the fitted linear ABC model given in object.
 #' @seealso [stats::summary.lm]
 #' @export
-summary.lmabc = function(object, ...){
-	ses = sqrt(diag(vcov(object)))  # this is the second column of the coefficient table in the output. i figured this out by running sqrt(diag(vcov(fit_base))), then seeing if there was any overlap with the output of summary(fit_base), which there was!
-	summary_base <- summary(object$lm)  # a lot of the information is the same between the base summary and the abc summary
+summary.lmabc = function(object, correlation = FALSE, symbolic.cor = FALSE, ...){
+	# New coefficient matrix
+	summary_base <- summary(object$lm) # a lot of the information is the same between the base summary and the abc summary
 
-	# if you examine the str() of a summary object—with the code that Kowal gave us, you could try str(summary(fit_base))—you'll see that it is a named list. The names are call, terms, residuals, etc. Look through this output and figure out what we can use and what we need to change. For instance, we can use r.squared, so there's no need to change that. BUT we can't use coefficients, so you'll need to assign a new coefficients matrix to that spot in the list. Here's an example to get you started: summary_base$coefficients <- (generate new coefficient matrix)
-	# eventually, we'll have to figure out if we can leave the class of summary_base as "summary.lm". We may have to create a new class, "summary.lmabc", if this doesn't work, along with print.summary.lmabc. But that's an issue for later! First let's get summary.lmabc working
+	ses = sqrt(diag(vcov(object))) # Calculating the new standard error
+	term_coeff = object$coefficients # extracting all the coefficents from the lmabc model
+	t_val = term_coeff/ses # New t-values
+	p_val = 2*pt(abs(t_val), object$df.residual, lower.tail = FALSE) # new probabilities calculated here
 
-	#fit_abc$coefficients
-	#sqrt(diag(stats::vcov(fit_abc)))
-	#fit_abc$coefficients/sqrt(diag(stats::vcov(fit_abc)))
-	#2*pt(abs(fit_abc$coefficients/sqrt(diag(stats::vcov(fit_abc)))), fit_abc$df.residual, lower.tail = FALSE)
+	summary_base$coefficients <- cbind(term_coeff, ses, t_val, p_val) # entering the new values of the coeff matrix
+	# renaming the dimensions of the new coeff matrix
+	dimnames(summary_base$coefficients)<-
+		list(names(term_coeff),
+				 c("Estimate", "Std. Error", "t value", "Pr(>|t|)"))
+
+	# New correlation matrix
+	if (correlation) {
+		rdf <- object$df.residual
+		p <- object$rank
+		p1 <- 1L:p
+		R <- chol2inv(object$qr$qr[p1, p1, drop = FALSE])
+		rss <- sum(object$residuals^2)
+		resvar <- rss/rdf
+
+		summary_base$correlation <- (R * resvar)/outer(ses, ses)
+		dimnames(summary_base$correlation) <- dimnames(summary_base$cov.unscaled)
+		summary_base$symbolic.cor <- symbolic.cor
+	}
+	# class of the returned object is still summary.lm, is this something we want
+	# to change?
+	class(summary_base) <- "summary.lmabc"
+	summary_base
 }
 
 #' @export
