30202227cee51d45c721d00c6bea6f2968091d54
Prayag Gordy <prayaggordy@gmail.com>
2022-07-01 18:51:17 -0400

    style getFullDesign

--- R/lmabc.R
+++ R/lmabc.R
@@ -235,53 +235,53 @@ getConstraints <- function(formula, data, cprobs = NULL) {
 }
 
 #' @export
-getFullDesign = function(formula, data, center = TRUE){
+getFullDesign <- function(formula, data, center = TRUE) {
 
 	# Model frame has some useful information
-	mf = stats::model.frame(formula = formula,
-													data  = data)
+	mf <- stats::model.frame(formula = formula,
+													 data = data)
 
 	# Names of the variables involved:
-	vnames = attr(mf, 'names')
+	vnames <- attr(mf, 'names')
 
 	# Covariates:
-	covar = attr(attr(mf, 'terms'),
-							 "term.labels")
+	covar <- attr(attr(mf, 'terms'),
+								"term.labels")
 
 	# Subset to the variables included in the model:
-	data = data[,vnames]
+	data <- data[,vnames]
 
 	# If specified, center the non-categorical variables
 	# note: do this *before* computing the interactions
-	if(center){
-		n_inds = which(sapply(data, is.numeric)) # numeric variables
-		n_inds = n_inds[-1] # remove the response
-		if(length(n_inds) > 0){ # if we have any numeric covariates...
-			dnames = names(data)[n_inds] # store the names
-			data[,n_inds] = data.frame(apply(data.frame(data[,n_inds]), 2,
-																			 function(d) d - mean(d, na.rm=TRUE))) # extra data.frame() statements in case there is only one variable
-			names(data)[n_inds] = dnames # restore the names
+	if (center) {
+		n_inds <- which(sapply(data, is.numeric))  # numeric variables
+		n_inds <- n_inds[-1]  # remove the response
+		if (length(n_inds) > 0) {  # if we have any numeric covariates...
+			dnames <- names(data)[n_inds]  # store the names
+			data[,n_inds] <- data.frame(apply(data.frame(data[,n_inds]), 2,
+																				function(d) d - mean(d, na.rm=TRUE)))  # extra data.frame() statements in case there is only one variable
+			names(data)[n_inds] <- dnames  # restore the names
 		}
 	}
 
 	# Handle the categorical variables:
-	f_inds = which(sapply(data, is.factor)) # factor indices
+	f_inds <- which(sapply(data, is.factor))  # factor indices
 
-	if(length(f_inds) > 0){
-		cdat = data.frame(data[,f_inds]) # data frame
-		cnames = names(cdat) = names(data)[f_inds] # correct the names
+	if (length(f_inds) > 0) {
+		cdat <- data.frame(data[,f_inds])  # data frame
+		cnames <- names(data)[f_inds]
+		names(cdat) <- names(data)[f_inds]  # correct the names
 
 		# Form the FULL (overparametrized) design  matrix:
 		# Note: same order as covar!
-		X = stats::model.matrix(object = formula,
-														data = data,
-														contrasts.arg =
-															lapply(cdat, stats::contrasts, contrasts=FALSE))
+		X <- stats::model.matrix(object = formula,
+														 data = data,
+														 contrasts.arg = lapply(cdat, stats::contrasts, contrasts=FALSE))
 	} else {
 		# No categorical variables, so just return the usual value:
-		X = stats::model.matrix(object = formula,
-														data = data)
+		X <- stats::model.matrix(object = formula,
+														 data = data)
 	}
 
-	return(X)
+	X
 }
