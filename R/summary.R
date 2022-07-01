fd46ba5768b055b38f6c25aabf524e2a20c87d0e
Prayag Gordy <prayaggordy@gmail.com>
2022-07-01 13:11:53 -0400

    add dots to print

--- R/summary.R
+++ R/summary.R
@@ -42,5 +42,5 @@ vcov.lmabc = function(object, ...){
 
 #' @export
 print.summary.lmabc <- function(x, digits = max(3L, getOption("digits") - 3L), symbolic.cor = x$symbolic.cor, signif.stars = getOption("show.signif.stars"), ...) {
-	stats:::print.summary.lm(x = x, digits = digits, symbolic.cor = symbolic.cor, signif.stars = signif.stars)
+	stats:::print.summary.lm(x = x, digits = digits, symbolic.cor = symbolic.cor, signif.stars = signif.stars, ...)
 }
