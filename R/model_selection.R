2438a338c20e5c5c658396d7cbb5007ec0753431
Virginia Baskin <virginiab@campchampions.com>
2022-07-05 15:19:03 -0500

    Fixed issue with BIC

--- R/model_selection.R
+++ R/model_selection.R
@@ -3,12 +3,6 @@ AIC.lmabc = function(object,...,k=2){
 	stats::AIC(object$lm,...,k=k)
 }
 
-# #' @export
-# BIC.lmabc = function(object,...){
-# 	stats::BIC(object$lm,...)
-# }
-
-#alternatively:
 #' @export
 BIC.lmabc = function(object,...){
 	stats::AIC(object$lm,..., k=log(stats::nobs(object$lm)))
