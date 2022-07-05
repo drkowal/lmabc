7d8e6bf72d491a978da4b45f425d33aae9dccdc9
Virginia Baskin <virginiab@campchampions.com>
2022-07-05 15:58:30 -0500

    Import BIC and nob

--- R/model_selection.R
+++ R/model_selection.R
@@ -3,7 +3,12 @@ AIC.lmabc = function(object, ..., k = 2){
 	stats::AIC(object$lm, ..., k = k)
 }
 
+## @export
+#BIC.lmabc = function(object, ...){
+#	stats::AIC(object$lm, ..., k = log(stats::nobs(object)))
+#}
+
 #' @export
 BIC.lmabc = function(object, ...){
-	stats::AIC(object$lm, ..., k = log(stats::nobs(object)))
+	stats::BIC(object$lm, ...)
 }
