93a6b7d34f09e5672ca0e70882113541072b5dee
Prayag Gordy <prayaggordy@gmail.com>
2022-07-06 11:39:01 -0400

    remove commented code

--- R/model_selection.R
+++ R/model_selection.R
@@ -3,11 +3,6 @@ AIC.lmabc = function(object, ..., k = 2){
 	stats::AIC(object$lm, ..., k = k)
 }
 
-## @export
-#BIC.lmabc = function(object, ...){
-#	stats::AIC(object$lm, ..., k = log(stats::nobs(object)))
-#}
-
 #' @export
 BIC.lmabc = function(object, ...){
 	stats::BIC(object$lm, ...)
