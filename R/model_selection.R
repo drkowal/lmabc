0d1e7f3f2caa489ec925310d8413ea12eb310b0f
Prayag Gordy <prayaggordy@gmail.com>
2022-10-20 13:06:32 -0500

    add logLik

--- R/model_selection.R
+++ R/model_selection.R
@@ -2,3 +2,8 @@
 logLik.lmabc <- function(object, REML = FALSE, ...) {
 	stats::logLik(object$lm, REML = REML, ...)
 }
+
+#' @export
+logLik.glmabc <- function(object, REML = FALSE, ...) {
+	stats::logLik(object$glm, REML = REML, ...)
+}
