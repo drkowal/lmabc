758a2952cdc22b63fa87dea9fd0cdfa331d3dd7e
Prayag Gordy <prayaggordy@gmail.com>
2022-10-20 12:58:20 -0500

    export print.glmabc

--- R/print.R
+++ R/print.R
@@ -4,6 +4,7 @@ print.lmabc <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
 	print.lm(x = x, digits = digits, ...)
 }
 
+#' @export
 print.glmabc <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
 	print.glm <- utils::getFromNamespace("print.glm", "stats")
 	print.glm(x = x, digits = digits, ...)
