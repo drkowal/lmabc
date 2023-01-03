fbb60763e878f9513979ade1d47cefb04a5f6dd0
Prayag Gordy <prayaggordy@gmail.com>
2023-01-03 18:46:34 -0500

    try mixed X

--- tests/testthat/test-lmabc.R
+++ tests/testthat/test-lmabc.R
@@ -3,6 +3,11 @@ test_that("lmabc works with f_contY_contX", {
 	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
 })
 
+test_that("lmabc works with f_contY_mixedX", {
+	f <- f_contY_mixedX
+	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
+})
+
 test_that("lmabc works with f_contY_catX", {
 	f <- f_contY_catX
 	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
