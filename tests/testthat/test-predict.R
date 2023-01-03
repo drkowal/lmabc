610b583049105c56233082106cb755ff57e66723
Prayag Gordy <prayaggordy@gmail.com>
2023-01-03 13:34:27 -0500

    first simple test

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -1,3 +1,6 @@
-test_that("predict.lm works", {
-  expect_equal(2 * 2, 4)
+test_that("predict.lmabc works with f_contY_contX", {
+	coefs <- as.matrix(coef(lmabc(f_contY_contX, df)))
+	X <- as.matrix(cbind(1, df[,rownames(coefs)[-1]]))
+	expect_equal((X %*% coefs)[,1],
+							 predict(lm(f_contY_contX, df), df))
 })
