24817263b4185c3fd94d12fed2c2036427f4cda7
Prayag Gordy <prayaggordy@gmail.com>
2023-01-03 13:35:39 -0500

    change title

--- tests/testthat/test-lmabc.R
+++ tests/testthat/test-lmabc.R
@@ -1,4 +1,4 @@
-test_that("predict.lmabc works with f_contY_contX", {
+test_that("lmabc works with f_contY_contX", {
 	coefs <- as.matrix(coef(lmabc(f_contY_contX, df)))
 	X <- as.matrix(cbind(1, df[,rownames(coefs)[-1]]))
 	expect_equal((X %*% coefs)[,1],
