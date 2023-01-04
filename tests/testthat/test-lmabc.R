34c5839d7c3e4f380aa6d6a8f1ec1c5f639e4e5a
Prayag Gordy <prayaggordy@gmail.com>
2023-01-03 19:54:23 -0500

    confirm no lm class

--- tests/testthat/test-lmabc.R
+++ tests/testthat/test-lmabc.R
@@ -4,9 +4,20 @@ test_that("lmabc has correct class in only continuous", {
 	expect_s3_class(lmabc(f, df), "lmabc")
 })
 
+test_that("lmabc does not have class 'lm' in only continuous", {
+	skip(message = "Current design decision means this will fail")
+	f <- f_contY_contX
+	expect_failure(expect_s3_class(lmabc(f, df), "lm"))
+})
+
 test_that("lmabc has correct class with some categoricals", {
 	f <- f_contY_catX
 	expect_s3_class(lmabc(f, df), "lmabc")
+})
+
+test_that("lmabc does not have class 'lm' with some categoricals", {
+	f <- f_contY_catX
+	expect_failure(expect_s3_class(lmabc(f, df), "lm"))
 })
 
 test_that("lmabc works with f_contY_contX", {
