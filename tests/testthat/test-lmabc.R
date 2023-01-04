61c5cd86ccfa9bfb6d4321189793bb999a34ca91
Prayag Gordy <prayaggordy@gmail.com>
2023-01-03 19:34:16 -0500

    add tests for more formulas

--- tests/testthat/test-lmabc.R
+++ tests/testthat/test-lmabc.R
@@ -1,4 +1,5 @@
 test_that("lmabc works with f_contY_contX", {
+	skip(message = "Current design decision means this will fail")
 	f <- f_contY_contX
 	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
 })
@@ -10,5 +11,26 @@ test_that("lmabc works with f_contY_mixedX", {
 
 test_that("lmabc works with f_contY_catX", {
 	f <- f_contY_catX
+	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
+})
+
+test_that("lmabc works with f_contY_contX.contX", {
+	skip(message = "Current design decision means this will fail")
+	f <- f_contY_contX.contX
+	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
+})
+
+test_that("lmabc works with f_contY_contX.catX", {
+	f <- f_contY_contX.catX
+	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
+})
+
+test_that("lmabc works with f_contY_catX.catX", {
+	f <- f_contY_catX.catX
+	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
+})
+
+test_that("lmabc works with f_contY_all", {
+	f <- f_contY_all
 	expect_equal(helper_fitted(f, df), lm(f, df)$fitted.values)
 })
