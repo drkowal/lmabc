ea96cc61ecb226834f78bc2bf1bf0705d79615d1
Prayag Gordy <prayaggordy@gmail.com>
2023-01-03 19:33:59 -0500

    handle interactions

--- tests/testthat/helper-lmabc.R
+++ tests/testthat/helper-lmabc.R
@@ -1,14 +1,14 @@
 helper_fitted <- function(f, df) {
-	all_terms <- attr(terms(f), "term.labels")
+	all_terms <- attr(terms(f), "term.labels")[which(attr(terms(f), "order")==1)]
+	df_used <- df[,all_terms]
 	factor_terms <- which(sapply(df_used, is.factor))
 	cont_terms <- setdiff(all_terms, names(factor_terms))
 
-	df_used <- df[,all_terms]
 	for (ct in cont_terms) {
 		df_used[,ct] <- scale(df_used[,ct], center = TRUE, scale = FALSE)
 	}
-
 	df_factors <- df_used[,factor_terms, drop = FALSE]
+
 	mm <- stats::model.matrix(object = f[-2],
 													  data = df_used,
 													  contrasts.arg = lapply(df_factors,
