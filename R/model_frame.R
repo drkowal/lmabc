b4cce363d5ff81b262d08c13f389671d6c2b856a
Prayag Gordy <prayaggordy@gmail.com>
2024-02-18 16:59:18 -0600

    document

--- R/model_frame.R
+++ R/model_frame.R
@@ -1,3 +1,14 @@
+#' Wrapper for model.frame
+#'
+#' `model.frame` returns a data.frame object for use in [lmabc()] and similar functions.
+#' It reimplements the first few lines of [lm()] to properly format the `formula` and `data` parameters.
+#' If only `formula` is passed, all variables must be vectors in the environment.
+#' If `formula` and `data` are passed, variables are first pulled from `data`, then filled from the environment.
+#'
+#' @param formula formula object
+#' @param data data.frame object
+#'
+#' @return a data.frame with the variables in `formula`
 model_frame <- function(formula, data) {
 	mf <- match.call(call = sys.call(which = -1))
 	m <- match(c("formula", "data"), names(mf), 0L)
