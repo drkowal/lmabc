07303bd964e0d46fd3e3bcc538edba359573aa7c
Prayag Gordy <prayaggordy@gmail.com>
2024-02-19 00:46:31 -0600

    test first

--- tests/testthat/test-getConstraints.R
+++ tests/testthat/test-getConstraints.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("y ~ cont1 + cont2 works", {
+  expect_null(getConstraints(f_contY_contX, df))
+})
+
+test_that("y ~ cat1 + cat2 works", {
+	expected <- matrix(nrow = 2, ncol = 7,
+										 dimnames = list(c("cyl", "gear"),
+										 								c("(Intercept)", "cyl4", "cyl6", "cyl8", "gear3", "gear4", "gear5")))
+
+	expect_equal(construct_empty_con(f_contY_catX, df), expected = expected)
 })
