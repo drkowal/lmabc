fcb1edfc21a10d773650cc5cf642e5d6edbca741
Prayag Gordy <prayaggordy@gmail.com>
2024-03-12 12:43:57 -0500

    first pass at fixing copied code

--- tests/testthat/test-glmabc.R
+++ tests/testthat/test-glmabc.R
@@ -1,19 +1,19 @@
-test_that("lmabc has correct class in only continuous", {
+test_that("glmabc has correct class in only continuous", {
 	f <- f_contY_contX
-	expect_s3_class(lmabc(f, df), "lmabc")
+	expect_s3_class(glmabc(f, df), c("glmabc", "lmabc"))
 })
 
-test_that("lmabc does not have class 'lm' in only continuous", {
+test_that("glmabc does not have class 'lm' in only continuous", {
 	f <- f_contY_contX
-	expect_failure(expect_s3_class(lmabc(f, df), "lm"))
+	expect_failure(expect_s3_class(glmabc(f, df), "glm"))
 })
 
-test_that("lmabc has correct class with some categoricals", {
+test_that("glmabc has correct class with some categoricals", {
 	f <- f_contY_catX
-	expect_s3_class(lmabc(f, df), "lmabc")
+	expect_s3_class(glmabc(f, df), c("glmabc", "lmabc"))
 })
 
-test_that("lmabc does not have class 'lm' with some categoricals", {
+test_that("glmabc does not have class 'lm' with some categoricals", {
 	f <- f_contY_catX
-	expect_failure(expect_s3_class(lmabc(f, df), "lm"))
+	expect_failure(expect_s3_class(glmabc(f, df), "glm"))
 })
