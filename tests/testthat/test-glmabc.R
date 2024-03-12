cd8201f616364137f3411dffc10b901f134a693a
Prayag Gordy <prayaggordy@gmail.com>
2024-03-12 13:03:19 -0500

    test coefs of glmabc with no cat

--- tests/testthat/test-glmabc.R
+++ tests/testthat/test-glmabc.R
@@ -17,3 +17,9 @@ test_that("glmabc does not have class 'glm' with some categoricals", {
 	f <- g_contY_catX
 	expect_failure(expect_s3_class(glmabc(f, family = "binomial", data = df), "glm"))
 })
+
+test_that("glmabc gives same non-intercept coefficients as glm with g_contY_contX", {
+	f <- g_contY_contX
+	expect_equal(coef(glmabc(f, family = "binomial", data = df))[-1],
+							 coef(glm(f, family = "binomial", data = df))[-1])
+})
