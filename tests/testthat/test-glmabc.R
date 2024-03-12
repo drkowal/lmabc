36d2f01f93ab90fc9d069b7dc853c26d601d8b9b
Prayag Gordy <prayaggordy@gmail.com>
2024-03-12 12:47:22 -0500

    add family

--- tests/testthat/test-glmabc.R
+++ tests/testthat/test-glmabc.R
@@ -1,19 +1,19 @@
 test_that("glmabc has correct class in only continuous", {
-	f <- f_contY_contX
-	expect_s3_class(glmabc(f, df), c("glmabc", "lmabc"))
+	f <- g_contY_contX
+	expect_s3_class(glmabc(f, family = "binomial", data = df), c("glmabc", "lmabc"))
 })
 
 test_that("glmabc does not have class 'lm' in only continuous", {
-	f <- f_contY_contX
-	expect_failure(expect_s3_class(glmabc(f, df), "glm"))
+	f <- g_contY_contX
+	expect_failure(expect_s3_class(glmabc(f, family = "binomial", data = df), "glm"))
 })
 
 test_that("glmabc has correct class with some categoricals", {
-	f <- f_contY_catX
-	expect_s3_class(glmabc(f, df), c("glmabc", "lmabc"))
+	f <- g_contY_catX
+	expect_s3_class(glmabc(f, family = "binomial", data = df), c("glmabc", "lmabc"))
 })
 
 test_that("glmabc does not have class 'lm' with some categoricals", {
-	f <- f_contY_catX
-	expect_failure(expect_s3_class(glmabc(f, df), "glm"))
+	f <- g_contY_catX
+	expect_failure(expect_s3_class(glmabc(f, family = "binomial", data = df), "glm"))
 })
