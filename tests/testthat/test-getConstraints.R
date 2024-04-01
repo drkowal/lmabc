c400d2cefc717e90fc7212712d0b05bf8b10d1c5
Prayag Gordy <prayaggordy@gmail.com>
2024-04-01 01:21:15 -0500

    clean naming

--- tests/testthat/test-getConstraints.R
+++ tests/testthat/test-getConstraints.R
@@ -1,4 +1,4 @@
-test_that("getConstraints works with no cat and no interactions", {
+test_that("getConstraints returns NULL with no cat and no interactions", {
   expect_null(getConstraints(f_contY_contX, df))
 })
 
